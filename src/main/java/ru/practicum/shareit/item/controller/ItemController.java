1e7c7a952bd06e3041eef2b68539d8c4096d4342
Alexander Petryakov <neTp9c@yandex.ru>
2023-10-10 21:39:21 +0300

    add aspect to log request

--- src/main/java/ru/practicum/shareit/item/controller/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/controller/ItemController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ru.practicum.shareit.common.LogInputOutputAnnotaion;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.mapper.ItemMapperImpl;
 import ru.practicum.shareit.item.model.Item;
@@ -25,24 +26,28 @@ public class ItemController {
     private final ItemMapperImpl itemMapper;
 
     @GetMapping
+    @LogInputOutputAnnotaion
     public List<ItemDto> findOwn(@RequestHeader("X-Sharer-User-Id") final Integer userId) {
         final List<Item> items = itemService.findByUserId(userId);
         return itemMapper.toItemDtoList(items);
     }
 
     @GetMapping("/{itemId}")
+    @LogInputOutputAnnotaion
     public ItemDto findById(@PathVariable final Integer itemId) {
         final Item item = itemService.findById(itemId);
         return itemMapper.toItemDto(item);
     }
 
     @GetMapping("/search")
+    @LogInputOutputAnnotaion
     public List<ItemDto> search(@RequestParam final String text) {
         final List<Item> items = itemService.search(text);
         return itemMapper.toItemDtoList(items);
     }
 
     @PostMapping
+    @LogInputOutputAnnotaion
     public ItemDto create(@RequestHeader("X-Sharer-User-Id") final Integer userId,
                           @RequestBody final ItemDto itemDto) {
         final Item item = itemMapper.toItem(itemDto);
@@ -53,6 +58,7 @@ public class ItemController {
     }
 
     @PatchMapping("/{itemId}")
+    @LogInputOutputAnnotaion
     public ItemDto update(@RequestHeader("X-Sharer-User-Id") final Integer userId,
                           @PathVariable final Integer itemId,
                           @RequestBody final ItemDto itemDto) {
--- src/main/java/ru/practicum/shareit/user/controller/UserController.java
+++ src/main/java/ru/practicum/shareit/user/controller/UserController.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit.user.controller;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.practicum.shareit.common.LogInputOutputAnnotaion;
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.mapper.UserMapper;
 import ru.practicum.shareit.user.model.User;
@@ -18,23 +20,30 @@ import ru.practicum.shareit.user.service.UserService;
 @RestController
 @RequestMapping(path = "/users")
 @RequiredArgsConstructor
+@Slf4j
 public class UserController {
     private final UserService userService;
     private final UserMapper userMapper;
 
     @GetMapping
+    @LogInputOutputAnnotaion
     public List<UserDto> findAll() {
+        log.info("Start to process findAll action from UserController with no parameters");
         final List<User> users = userService.findAll();
-        return userMapper.toUserDtoList(users);
+        final List<UserDto> usersDto = userMapper.toUserDtoList(users);
+        log.info("End to process findAll action from UserController with result: {}", usersDto);
+        return usersDto;
     }
 
     @GetMapping("/{userId}")
+    @LogInputOutputAnnotaion
     public UserDto findById(@PathVariable final Integer userId) {
         final User user = userService.findById(userId);
         return userMapper.toUserDto(user);
     }
 
     @PostMapping
+    @LogInputOutputAnnotaion
     public UserDto create(@RequestBody final UserDto userDto) {
         final User user = userMapper.toUser(userDto);
         final User createdUser = userService.create(user);
@@ -42,6 +51,7 @@ public class UserController {
     }
 
     @PatchMapping("/{userId}")
+    @LogInputOutputAnnotaion
     public UserDto update(@PathVariable final Integer userId,
                           @RequestBody final UserDto userDto) {
         final User user = userMapper.toUser(userDto);
@@ -52,6 +62,7 @@ public class UserController {
     }
 
     @DeleteMapping("/{userId}")
+    @LogInputOutputAnnotaion
     public void delete(@PathVariable final Integer userId) {
         userService.delete(userId);
     }
