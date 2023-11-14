91f82f21ab3db37123dfbc3d64372361d9db86ba
Alexander Petryakov <neTp9c@yandex.ru>
2023-11-14 20:29:37 +0300

    remove unnecessary EnableJpaRepositories

--- src/main/java/ru/practicum/shareit/Config.java
+++
@@ -1,9 +0,0 @@
-package ru.practicum.shareit;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-@Configuration
-@EnableJpaRepositories
-public class Config {
-}
