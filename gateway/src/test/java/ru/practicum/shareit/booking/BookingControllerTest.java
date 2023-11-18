feb42b0047f7cd1bcdeb8ada4713ed48a60dc4c3
Alexander Petryakov <neTp9c@yandex.ru>
2023-11-18 23:24:00 +0300

    auto fix format

--- gateway/src/test/java/ru/practicum/shareit/booking/BookingControllerTest.java
+++ gateway/src/test/java/ru/practicum/shareit/booking/BookingControllerTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
